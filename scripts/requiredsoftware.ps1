690fbc7f56cf279301a5769e48a69ce3d8c8e800
makerling <makerling@users.noreply.github.com>
2019-07-04 10:07:55 +0300

    Create requiredsoftware.ps1

---
+++ scripts/requiredsoftware.ps1
@@ -0,0 +1,7 @@
+#installing dependencies for dev environment via chocolatey
+
+cinst chocolateygui -y
+cinst dotnetfx -y
+cinst git.install -y
+cinst Google Chrome -y
+cinst visualstudio2017community -y
