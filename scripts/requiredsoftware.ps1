dc5b6888e34370c2fd11fdfaf6aa7ec1006c0911
makerling <makerling@users.noreply.github.com>
2019-07-04 13:10:25 +0300

    Update requiredsoftware.ps1

--- scripts/requiredsoftware.ps1
+++ scripts/requiredsoftware.ps1
@@ -5,3 +5,6 @@ cinst dotnetfx -y
 cinst git.install -y
 cinst Google Chrome -y
 cinst visualstudio2017community -y
+
+#default location vs studio 2017
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"
