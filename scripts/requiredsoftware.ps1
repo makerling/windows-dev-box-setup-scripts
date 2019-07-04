e0304292353d3301a589dbd40eebd177ec1bf801
makerling <makerling@users.noreply.github.com>
2019-07-04 14:10:53 +0300

    Update requiredsoftware.ps1

--- scripts/requiredsoftware.ps1
+++ scripts/requiredsoftware.ps1
@@ -6,5 +6,43 @@ cinst git.install -y
 cinst Google Chrome -y
 cinst visualstudio2017community -y
 
-#default location vs studio 2017
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"
+#installing workloads/components for building FLEx9
+#default vsinstaller location vs 2017
+$vsStudioInstallInstaller = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"
+
+#workload/component IDs 
+#from here: https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community?vs-2017&view=vs-2017
+
+#.NET desktop development workload
+$wkload1ID = "Microsoft.VisualStudio.Workload.ManagedDesktop"
+
+#Desktop development with C++ workload
+$wkload2ID = "Microsoft.VisualStudio.Workload.NativeDesktop"
+
+#Python development workload
+$wkload3ID = "Microsoft.VisualStudio.Workload.Python"
+
+#.NET Core cross-platform development workload
+$wkload4ID = "Microsoft.VisualStudio.Workload.NetCoreTools"
+
+#Universal Windows Platform development workload
+$wkload5ID = "Microsoft.VisualStudio.Workload.Universal"
+
+#Windows Universal CRT SDK component
+$cmp1ID = "Microsoft.Component.VC.Runtime.UCRTSDK"
+
+#Windows 8.1 SDK component
+$cmp2ID = "Microsoft.VisualStudio.Component.Windows81SDK"
+
+#https://stackoverflow.com/questions/2608144/how-to-split-long-commands-over-multiple-lines-in-powershell
+{& $vsStudioInstallInstaller @Args} | % Invoke @(
+  '--passive'
+  '--includeRecommended'
+  '--add ${wkload1ID}'
+  '--add ${wkload2ID}'
+  '--add ${wkload3ID}'
+  '--add ${wkload4ID}'
+  '--add ${wkload5ID}'
+  '--add ${cmp1ID}'
+  '--add ${cmp2ID}'
+)
