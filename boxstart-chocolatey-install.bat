32b96c0e22d6bfbab1d85f53fce2173874713b37
makerling <makerling@users.noreply.github.com>
2019-07-04 14:59:18 +0300

    Update boxstart-chocolatey-install.bat

--- boxstart-chocolatey-install.bat
+++ boxstart-chocolatey-install.bat
@@ -1,9 +1,10 @@
 @echo off
 
-:: installing Boxstarter from web and chocolatey 
+REM installing Boxstarter from web and chocolatey 
 
 powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force"
 
+powershell -NoProfile -ExecutionPolicy bypass -command ". '%~dp0dev_app.ps1'"
 
 echo "finished!!"
 
