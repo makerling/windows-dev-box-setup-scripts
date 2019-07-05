474ab5b0cf40dad4e5aee8774568900af8a7383c
makerling <makerling@users.noreply.github.com>
2019-07-05 14:47:08 +0300

    Update boxstart-chocolatey-install.bat

--- boxstart-chocolatey-install.bat
+++ boxstart-chocolatey-install.bat
@@ -6,7 +6,7 @@ powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://bo
 
 REM powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -PackageName https://gist.githubusercontent.com/makerling/6af68b1c6bc636549b36ec19c41575d7/raw/3d924ff1b7ea9000b2c310ae07d04b7ed1674ce0/FLEx9-dev-setup.ps1"
 REM no reboot boxstarter parameter
-powershell -NoProfile -ExecutionPolicy bypass -command "START http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/makerling/6af68b1c6bc636549b36ec19c41575d7/raw/3d924ff1b7ea9000b2c310ae07d04b7ed1674ce0/FLEx9-dev-setup.ps1"
+powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/makerling/6af68b1c6bc636549b36ec19c41575d7/raw/3d924ff1b7ea9000b2c310ae07d04b7ed1674ce0/FLEx9-dev-setup.ps1 }" 
 
 echo "finished!!"
 
